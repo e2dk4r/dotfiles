f7be843fd329c698c40b35b92d5043cd575ecdaa
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2025-03-09 16:49:22 +0300

    wine: refactor
    - Clean reg add commands
    - Do not depend on Gentoo specific scripts.
    - Remove unnecessary variables

--- wine/wine_config.sh
+++ wine/wine_config.sh
@@ -1,6 +1,2 @@
 export WINEPREFIX=/tmp/gaming/wine
-export WINEFSYNC=1
-export DOSDEVICE_Z="${WINEPREFIX%/*}"
-
 export WINEHOME="$WINEPREFIX/drive_c/users/$USER"
-export WINEAPPDATADIR="$WINEHOME/AppData/Local"
--- wine/wine_setup.sh
+++ wine/wine_setup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 export WINEPREFIX=/tmp/gaming/wine
-export WINEFSYNC=1
 export DOSDEVICE_Z="${WINEPREFIX%/*}"
-
-export WINEHOME="$WINEPREFIX/drive_c/users/$USER"
-export WINEAPPDATADIR="$WINEHOME/AppData/Local"
 
 wineboot
 # wineboot doesn't wait for prefix to be ready
@@ -40,7 +36,7 @@ echo '  # run with higher io scheduler priority'
 echo '  ionice -c 2 -n 0 wine game.exe'
 
 # https://wiki.winehq.org/FAQ#How_can_I_prevent_Wine_from_changing_the_filetype_associations_on_my_system_or_adding_unwanted_menu_entries.2Fdesktop_links.3F
-wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\DllOverrides /v winemenubuilder.exe /t REG_SZ /d \"\" /f" >/dev/null 2>&1
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v winemenubuilder.exe /t REG_SZ /d '' /f >/dev/null 2>&1
 
 # https://wiki.winehq.org/Useful_Registry_Keys
 # HKEY_CURRENT_USER\Software\Wine\Drivers\Graphics
@@ -49,17 +45,17 @@ wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\DllOverrides /v winemenu
 #    x11:     Use the X11 driver
 #    wayland: Use the Wayland driver
 #    null:    Use the null driver (a virtual screen will be created, but not displayed; available since Wine 5.2)
-wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\Drivers /v Graphics /t REG_SZ /d wayland /f" >/dev/null 2>&1
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /t REG_SZ /d wayland /f >/dev/null 2>&1
 
 # The Last of Us Part I
 ## texture settings locked
 ## solution #1: use wine's amd_ags_x64
-wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\AppDefaults\\tlou-i.exe\\DllOverrides /v amd_ags_x64 /t REG_SZ /d builtin /f" >/dev/null 2>&1
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\AppDefaults\tlou-i.exe\DllOverrides' /v amd_ags_x64 /t REG_SZ /d builtin /f >/dev/null 2>&1
 
 # Uncharted Legacy of Thieves Collection
 ## exits with 'initGpu() failed'
 ## solution #1: use wine's amd_ags_x64
-wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\AppDefaults\\u4.exe\\DllOverrides /v amd_ags_x64 /t REG_SZ /d builtin /f" >/dev/null 2>&1
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\AppDefaults\u4.exe\DllOverrides' /v amd_ags_x64 /t REG_SZ /d builtin /f >/dev/null 2>&1
 ## solution #2: spoof nvidia card
 ## ```dxvk.conf
 ## [u4.exe]
@@ -69,13 +65,22 @@ wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\AppDefaults\\u4.exe\\Dll
 # RoboCop Rouge City
 ## mouse events not registered
 ## solution #1: use virtual screen
-wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\Explorer\\Desktops /v 1080p /t REG_SZ /d 1920x1080 /f" >/dev/null 2>&1
-wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\AppDefaults\\RoboCop-Win64-Shipping.exe\\Explorer /v Desktop /t REG_SZ /d 1080p /f" >/dev/null 2>&1
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\Explorer\Desktops' /v 1080p /t REG_SZ /d 1920x1080 /f >/dev/null 2>&1
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\AppDefaults\RoboCop-Win64-Shipping.exe\Explorer' /v Desktop /t REG_SZ /d 1080p /f >/dev/null 2>&1
 
 # Remnant 2
 ## mouse events not registered
 ## solution #1: use virtual screen
-wine cmd /c "reg add HKEY_CURRENT_USER\\Software\\Wine\\AppDefaults\\Remnant2-Win64-Shipping.exe\\Explorer /v Desktop /t REG_SZ /d 1080p /f" >/dev/null 2>&1
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\AppDefaults\Remnant2-Win64-Shipping.exe\Explorer' /v Desktop /t REG_SZ /d 1080p /f >/dev/null 2>&1
 
-setup_dxvk.sh install
-setup_vkd3d_proton.sh install
+cp /usr/lib/dxvk/x64/*.dll "$WINEPREFIX/drive_c/windows/system32"
+cp /usr/lib/dxvk/x32/*.dll "$WINEPREFIX/drive_c/windows/syswow64"
+for dll in d3d8 d3d9 d3d10core d3d11 dxgi; do
+  wine reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v $dll /t REG_SZ /d native /f >/dev/null 2>&1
+done
+
+cp /usr/lib/vkd3d-proton/x64/*.dll "$WINEPREFIX/drive_c/windows/system32"
+cp /usr/lib/vkd3d-proton/x32/*.dll "$WINEPREFIX/drive_c/windows/syswow64"
+for dll in d3d12 d3d12core; do
+  wine reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v $dll /t REG_SZ /d native /f >/dev/null 2>&1
+done
